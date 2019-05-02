76cf3003ce8d667b36aaf4745772a50cc580508a
johnmwangi <jonesmwas356@gmail.com>
2019-05-02 08:03:52 +0300

    start hidden

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/john/Desktop/Python/Pitches/virtual"
+set -gx VIRTUAL_ENV "/home/john/Desktop/Python/blogg/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
